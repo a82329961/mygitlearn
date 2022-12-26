2f6a1243f35ef5149aad4fd9fd91149bcece2830
xiechao <a82329961@163.com>
2022-12-26 14:11:26 +0800

    init

---
+++ src/main/java/com/ius/TestClass.java
@@ -0,0 +1,12 @@
+package com.ius;
+
+/**
+ * @Description: test git
+ * @author: Admin
+ * @date: 2022年12月26日 14:09
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
